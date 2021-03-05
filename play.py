4937a97f343c83a8764157d6d27d005d8c5efe5e
linzhank <linzhank@gmail.com>
2021-03-05 17:10:19 -0500

    fix bugs in play

--- play.py
+++ play.py
@@ -6,8 +6,8 @@ import sys
 import os
 import time
 import numpy as np
-import tensorflow as tf
 import cv2
+import tensorflow as tf
 from datetime import datetime
 import logging
 logging.basicConfig(format='%(asctime)s %(message)s',level=logging.DEBUG)
@@ -20,6 +20,7 @@ dim_latent = 16
 dim_view = (128,128,1)
 num_act = 10
 dim_act = 1
+version='macromphalus'
 total_steps = 100
 
 # Get camera ready
@@ -38,20 +39,20 @@ wheels = MecanumDriver() # need integrate mecdriver into agent in next version
 # Get agent ready
 brain = IntrinsicMotivationAgent(dim_view, dim_latent, num_act, dim_act)
 version = 'macromphalus'
-load_dir = os.path.join(sys.path[0], 'model_dir', version, '2021-03') # typically use the last saved models
-brain.vae.encoder.encoder_net = tf.keras.models.load_model(os.path.join(load_dir, 'encoder'))
-brain.vae.decoder.decoder_net = tf.keras.models.load_model(os.path.join(load_dir, 'decoder'))
-brain.imaginator.dynamics_net = tf.keras.models.load_model(os.path.join(load_dir, 'imaginator'))
-brain.ac.actor.policy_net = tf.keras.models.load_model(os.path.join(load_dir, 'actor'))
-brain.ac.critic.value_net = tf.keras.models.load_model(os.path.join(load_dir, 'critic'))
+model_dir = os.path.join(sys.path[0], 'model_dir', version, '2021-03-05-16-00') # typically use the last saved models
+brain.vae.encoder.encoder_net = tf.keras.models.load_model(os.path.join(model_dir, 'encoder'))
+brain.vae.decoder.decoder_net = tf.keras.models.load_model(os.path.join(model_dir, 'decoder'))
+brain.imaginator.dynamics_net = tf.keras.models.load_model(os.path.join(model_dir, 'imaginator'))
+brain.ac.actor.policy_net = tf.keras.models.load_model(os.path.join(model_dir, 'actor'))
+brain.ac.critic.value_net = tf.keras.models.load_model(os.path.join(model_dir, 'critic'))
 memory = OnPolicyBuffer(max_size=total_steps)
 # Generate first imagination and action
 ret, frame = eye.read() # obs = env.reset()
 view = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)/255. # from 0~255 to 0~1
 view.resize(1,128,128,1)
-obs, _ = brain.vae.encode(view) # encoded distribution
-act, val, lpa = brain.ac.make_decision(obs) 
-mu_imn, logsigma_imn = brain.imaginator(obs, tf.reshape(act, (1,1))) # imagined latent state
+obs, _ = brain.vae.encoder(view) # encoded distribution
+act, val, lpa = brain.ac.make_decision(tf.expand_dims(obs,0)) 
+mu_imn, logsigma_imn = brain.imaginator(tf.expand_dims(obs,0), tf.reshape(act, (1,1))) # imagined latent state
 wheels.set_action(int(act))
 logging.info("\n====Get Ready====\n")
 # Preapare for experience collecting
@@ -84,28 +85,28 @@ try:
             memory.store(obs, act, rew, val, lpa, mu, logsigma)
             step_counter+=1
             stepwise_frames.append(frame_counter)
-            logging.debug("\nepisode: {} \nstep: {} \ncurrent state: {} \nimagination: {} \naction: {} \nnext state: {} \nvalue: {} \nlog prob: {} \nreward: {} \nepisode return: {} \nepisode length: {}".format(episode_counter+1, step_counter, obs, (mu_imn, logsigma_imn), act, (mu, logsigma), val, logp, rew, ep_ret, ep_len))
+            logging.debug("\nepisode: {} \nstep: {} \ncurrent state: {} \nimagination: {} \naction: {} \nnext state: {} \nvalue: {} \nlog prob: {} \nreward: {} \nepisode return: {} \nepisode length: {}".format(episode_counter+1, step_counter, obs, (mu_imn, logsigma_imn), act, (mu, logsigma), val, lpa, rew, ep_ret, ep_len))
             # handle episode terminal
             if not step_counter%max_ep_len:
-                _, val, _ = brain.make_decision(obs)
+                _, val, _ = brain.ac.make_decision(tf.expand_dims(obs,0))
                 memory.finish_path(val)
                 episode_counter+=1
                 episodic_returns.append(ep_ret)
                 logging.info("\n----\nTotalFrames: {} \nEpisode: {}, EpReturn: {} \n----\n".format(frame_counter, episode_counter, ep_ret))
                 ep_ret, ep_len = 0, 0
             # compute next obs, act, val, logp
-            obs = mu.copy() # SUPER CRITICAL!!!
-            act, val, logp = brain.make_decision(obs) 
+            obs = mu.numpy().copy() # SUPER CRITICAL!!!
+            act, val, lpa = brain.ac.make_decision(tf.expand_dims(obs,0)) 
             wheels.set_action(int(act))
-            mu_imn, logsigma_imn = brain.imaginator(obs, np.reshape(act, (1,1)))
+            mu_imn, logsigma_imn = brain.imaginator(tf.expand_dims(obs,0), np.reshape(act, (1,1)))
             
     # Save valuable items
-    with open(os.path.join(save_dir, 'elapsed_time.txt'), 'w') as f:
+    with open(os.path.join(os.path.dirname(model_dir), 'elapsed_time.txt'), 'w') as f:
         f.write("{}".format(time.time()-start_time))
     replay_data = memory.get()
-    np.save(os.path.join(save_dir, 'replay_data.npy'), replay_data)
-    np.save(os.path.join(save_dir, 'stepwise_frames.npy'), stepwise_frames)
-    np.save(os.path.join(save_dir, 'episodic_returns.npy'), episodic_returns)
+    np.save(os.path.join(os.path.dirname(model_dir), 'replay_data.npy'), replay_data)
+    np.save(os.path.join(os.path.dirname(model_dir), 'stepwise_frames.npy'), stepwise_frames)
+    np.save(os.path.join(os.path.dirname(model_dir), 'episodic_returns.npy'), episodic_returns)
 except KeyboardInterrupt:    
     print("\r\nctrl + c:")
     eye.release()
