d233a157785266be1e0b73fb4658c6178ea308bd
linzhank <linzhank@gmail.com>
2021-03-05 17:36:15 -0500

    fix bugs in ignite

--- ignite.py
+++ ignite.py
@@ -12,6 +12,7 @@ import tensorflow as tf
 import logging
 logging.basicConfig(format='%(asctime)s %(message)s',level=logging.INFO)
 
+from drivers.mecanum_driver import MecanumDriver
 from agents.ima_macromphalus import IntrinsicMotivationAgent
 
 
@@ -25,7 +26,7 @@ time_stamp = datetime.now().strftime("%Y-%m-%d-%H-%M")
 # Instantiate camera, motor driver, agent
 eye = cv2.VideoCapture("nvarguscamerasrc ! video/x-raw(memory:NVMM), width=(int)128, height=(int)128, format=(string)NV12, framerate=(fraction)30/1 ! nvvidconv flip-method=0 ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink", cv2.CAP_GSTREAMER)
 eye.get(cv2.CAP_PROP_FPS)
-image_dir = os.path.join('/ssd', version+'_experience', time_stamp, 'views')
+image_dir = os.path.join('/ssd', versions[1]+'_experience', time_stamp, 'views')
 if not os.path.exists(image_dir):
     try:
         os.makedirs(image_dir)
@@ -35,9 +36,12 @@ if not os.path.exists(image_dir):
 wheels = MecanumDriver() # need integrate mecdriver into agent in next version
 brain = IntrinsicMotivationAgent(dim_view, dim_latent, num_act, dim_act)
 # Data collection
+frame_counter = 0
+step_counter = 0
+time_elapse = 0
 start_time = time.time()
 try:
-    while step_counter < 10:
+    while step_counter < 1000:
         ret, frame = eye.read()
         cv2.imwrite(os.path.join(image_dir, str(frame_counter)+'.jpg'), frame)
         prev_time_elapse = time_elapse
@@ -45,12 +49,17 @@ try:
         frame_counter+=1
         if int(time_elapse)-int(prev_time_elapse): # take an action every sec
             wheels.set_action(int(np.random.randint(low=0, high=num_act)))
+            step_counter += 1
 except KeyboardInterrupt:    
     print("\r\nctrl + c:")
     eye.release()
     cv2.destroyAllWindows()
     wheels.halt()
     exit()
+# When everything done, release the capture and stop motors
+eye.release()
+cv2.destroyAllWindows()
+wheels.halt()
 
 # Set model paths
 model_dir = os.path.join(sys.path[0], 'model_dir', versions[1], time_stamp)
@@ -67,3 +76,5 @@ brain.vae.decoder.decoder_net.save(decoder_path)
 brain.imaginator.dynamics_net.save(imaginator_path)
 brain.ac.actor.policy_net.save(actor_path)
 brain.ac.critic.value_net.save(critic_path)
+
+
