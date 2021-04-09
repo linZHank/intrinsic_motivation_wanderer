fa575d5ebc037334e10fb88a42cc2cba2507312e
linzhank <linzhank@gmail.com>
2021-04-09 18:54:54 -0400

    update ignite

--- ignite.py
+++ ignite.py
@@ -13,7 +13,7 @@ import logging
 logging.basicConfig(format='%(asctime)s %(message)s',level=logging.INFO)
 
 from drivers.mecanum_driver import MecanumDriver
-from agents.ima_macromphalus import IntrinsicMotivationAgent
+from agents.ima_macromphalus import IntrinsicMotivationAgent, OnPolicyBuffer
 
 
 # Parameters
@@ -24,9 +24,6 @@ num_act = 10
 dim_act = 1
 total_steps = 300
 time_stamp = datetime.now().strftime("%Y-%m-%d-%H-%M")
-# Instantiate camera, motor driver, agent
-eye = cv2.VideoCapture("nvarguscamerasrc ! video/x-raw(memory:NVMM), width=(int)128, height=(int)128, format=(string)NV12, framerate=(fraction)30/1 ! nvvidconv flip-method=0 ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink", cv2.CAP_GSTREAMER)
-eye.get(cv2.CAP_PROP_FPS)
 image_dir = os.path.join('/ssd', versions[1]+'_experience', time_stamp, 'views')
 if not os.path.exists(image_dir):
     try:
@@ -34,33 +31,54 @@ if not os.path.exists(image_dir):
     except OSError as e:
         if e.errno != errno.EEXIST:
             raise
+# Instantiate camera, motor driver, agent, buffer
+eye = cv2.VideoCapture("nvarguscamerasrc ! video/x-raw(memory:NVMM), width=(int)128, height=(int)128, format=(string)NV12, framerate=(fraction)30/1 ! nvvidconv flip-method=0 ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink", cv2.CAP_GSTREAMER)
+eye.get(cv2.CAP_PROP_FPS)
 wheels = MecanumDriver() # need integrate mecdriver into agent in next version
-brain = IntrinsicMotivationAgent(dim_view, dim_latent, num_act, dim_act)
+brain = IntrinsicMotivationAgent(dim_view=dim_view, dim_latent=dim_latent, num_act=num_act, dim_act=dim_act)
+memory = OnPolicyBuffer(max_size=total_steps)
 # Ignition parameters
 frame_counter = 0
 step_counter = 0
 time_elapse = 0
-stepwise_frames = []
+stepwise_frame_ids = []
 action_data = np.zeros(shape=(total_steps,), dtype=np.float32)
 
 # Start data collection
 start_time = time.time()
 ret, frame = eye.read()
-cv2.imwrite(os.path.join(image_dir, str(frame_counter)+'.jpg'), frame)
+view = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)/255. # from 0~255 to 0~1
+view.resize(1,128,128,1)
+mu, logsigma = brain.vae.encoder(img)
+obs = brain.vae.reparameterize(mu, logsigma)
 act = np.random.randint(low=0, high=num_act)
+imn_mu, imn_logsigma = brain.imaginator(tf.expand_dims(obs,0), tf.reshape(act,(1,1)))
+imn_obs = brain.imaginator.reparameterize(imn_mu, imn_logsigma)
 wheels.set_action(act)
 try:
     while step_counter < total_steps:
         prev_time_elapse = time_elapse
         time_elapse = time.time() - start_time
-        frame_counter+=1
         ret, frame = eye.read()
         cv2.imwrite(os.path.join(image_dir, str(frame_counter)+'.jpg'), frame)
+        frame_counter+=1
         if int(time_elapse)-int(prev_time_elapse): # take a random action in every sec
-            action_data[step_counter] = act
-            stepwise_frames.append(frame_counter)
+            nxt_view = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)/255.
+            nxt_view.resize(1,128,128,1)
+            nxt_mu, nxt_logsigma = brain.vae.encoder(nxt_view)
+            nxt_obs = brain.vae.reparameterize(nxt_mu, nxt_logsigma)
+            rew = brain.compute_intrinsic_reward(nxt_mu, nxt_logsigma, imn_obs)
+            memory.store(
+                obs, act, rew, 0, 0, nxt_obs, imn_obs, mu, logsigma, nxt_mu, nxt_logsigma, imn_mu, imn_logsigma
+            )
+            stepwise_frame_ids.append(frame_counter)
             step_counter += 1
+            obs = nxt_obs
+            mu = nxt_mu
+            logsigma = nxt_logsigma
             act = np.random.randint(low=0, high=num_act)
+            imn_mu, imn_logsigma = brain.imaginator(tf.expand_dims(obs,0), tf.reshape(act,(1,1)))
+            imn_obs = brain.imaginator.reparameterize(imn_mu, imn_logsigma)
             wheels.set_action(act)
 except KeyboardInterrupt:    
     print("\r\nctrl + c:")
